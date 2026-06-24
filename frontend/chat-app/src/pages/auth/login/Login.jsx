e5f9ef099eb61941be9c08b6fac7f1fa9ab21584
Farrukhsmit11 <farrukhmoten35@gmail.com>
2026-06-24 16:27:32 +0500

    created authentication forms

--- frontend/chat-app/src/pages/auth/login/Login.jsx
+++ frontend/chat-app/src/pages/auth/login/Login.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import AuthNav from '../../../components/authNav/AuthNav'
+import { Formik } from "formik"
+import { Button, Checkbox, Form, Input } from "antd"
+import "./Login.css"
 
 const Login = () => {
+
+  const initialValues = {
+    email: "",
+    password: ""
+  }
+
+  const [form] = Form.useForm()
+
   return (
-    <div className='auth-container'>
-      <div className="auth-card">
+    <>
+      <AuthNav />
+      <div className='auth-container'>
+        <div className="auth-card">
+          <div className="auth-header">
+            <h1 className='auth-title'>Welcome Back 👋</h1>
+            <span className='auth-description'>Sign in to continue your conversations.</span>
+          </div>
+
+          <Formik
+            initialValues={initialValues}
+          >
+            {({
+              handleBlur,
+              handleSubmit,
+              handleChange
+            }) => (
+              <Form form={form} layout='vertical'>
+                <Form.Item label="Email">
+                  <Input className='form-input' placeholder='Email Address'></Input>
+                </Form.Item>
+
+                <Form.Item label="Password">
+                  <Input.Password className='form-input' placeholder='Password'></Input.Password>
+                </Form.Item>
+
+                <div className='footer'>
+                  <Checkbox>Remember me</Checkbox>
+                  <a href='#'>Forgot Password</a>
+                </div>
+
+                <div className="auth-login-footer">
+                  <Button className='submit-btn'>Log in</Button>
+                </div>
+
+              </Form>
+            )
+            }
+          </Formik>
+        </div>
       </div>
-    </div>
+    </>
+
   )
 }
 
--- frontend/chat-app/src/pages/auth/signUp/Signup.jsx
+++ frontend/chat-app/src/pages/auth/signUp/Signup.jsx
@@ -1,15 +1,89 @@
-import React from 'react'
-import "./Signup.css"
+import React, { useState } from 'react'
+import "../Auth.css"
+import { Button, Form, Input } from "antd"
+import { Formik } from "formik"
+import AuthNav from '../../../components/authNav/AuthNav'
+import { useNavigate } from "react-router-dom"
 
 const Signup = () => {
 
     const [form] = Form.useForm()
 
+    const [loading, setLoading] = useState(false)
+
+    const initialvalues = {
+        name: "",
+        email: "",
+        password: ""
+    }
+
+    const onSubmit = (values) => {
+        console.log(values)
+        setLoading(true)
+    }
+
+    const navigate = useNavigate();
+
     return (
         <>
+            <AuthNav />
             <div className='auth-container'>
                 <div className="auth-card">
-                    <h1>Sign Up</h1>
+
+                    <div className="auth-header">
+                        <h1 className='auth-title'>Create Account</h1>
+                    </div>
+
+                    <Formik
+                        initialvalues={initialvalues}
+                        onSubmit={onSubmit}
+                    >
+                        {({
+                            handleSubmit,
+                            handleReset,
+                            handleChange,
+                            values,
+                            errors,
+                            touched
+
+                        }) => (
+                            <Form form={form} layout='vertical' onFinish={handleSubmit}>
+                                <Form.Item label="Full Name">
+                                    <Input
+                                        className='form-input'
+                                        placeholder='Full name'
+                                    >
+                                    </Input>
+                                </Form.Item>
+
+                                <Form.Item label="Email">
+                                    <Input
+                                        className='form-input'
+                                        placeholder='Email Address'
+                                    >
+                                    </Input>
+                                </Form.Item>
+
+                                <Form.Item label="Password">
+                                    <Input.Password
+                                        className='form-input'
+                                        placeholder='Password'
+                                    >
+                                    </Input.Password>
+                                </Form.Item>
+
+                                <div className='auth-footer'>
+                                    <Button className='submit-btn' loading={loading} htmlType='submit'>Sign Up</Button>
+                                    <Button
+                                        onClick={() => navigate("/login")}
+                                        className='submit-btn-black'
+                                    >Log in</Button>
+                                </div>
+                            </Form>
+                        )
+                        }
+                    </Formik>
+
                 </div>
             </div>
         </>
