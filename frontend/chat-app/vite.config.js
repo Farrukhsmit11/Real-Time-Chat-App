f0d91d3ba05ec9a5ea0d766411bba74fa85f3dbb
Farrukhsmit11 <farrukhmoten35@gmail.com>
2026-09-16 19:42:41 +0500

    frontend issue fixed for deployment

--- frontend/chat-app/vite.config.js
+++ frontend/chat-app/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['real-time-chat-app-production-0408.up.railway.app']
+  }
 })
